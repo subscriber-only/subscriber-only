a21070038c0a0e8259de8b9a83c7c83324a4a263
Vitor <vmsousapereira@gmail.com>
2023-05-01 18:53:46 +0100

    Update host configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailjet_api
 
-  config.action_mailer.default_url_options = {
-    host: "app.subscriber-only.com",
-    port: 443,
-  }
-  routes.default_url_options[:host] = "app.subscriber-only.com:443"
+  config.action_mailer.default_url_options = { host: "app.subscriber-only.com" }
+  routes.default_url_options[:host] = "app.subscriber-only.com"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
